8ee3a433bc478aa463c9f313e6f06599642b6be5
PZiamanesh <po.ziamanesh@gmail.com>
2022-10-22 10:59:38 +0330

    Add project files.

---
+++ RazorPageApp/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public RedirectToPageResult OnGetLoad()
+        {
+           return RedirectToPage("./Privacy");
+        }
+    }
+}
